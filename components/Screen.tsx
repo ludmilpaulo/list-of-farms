2a01d23536d84af75c1dd9e9ffe2d2edc3e1418c
Ludmil Paulo <53780546+ludmilpaulo@users.noreply.github.com>
2022-10-25 16:18:16 +0200

    message

--- components/Screen.tsx
+++ components/Screen.tsx
@@ -1,25 +1,26 @@
-import React from 'react'
-import { SafeAreaView, StyleSheet, View, ImageBackground } from 'react-native'
-import Constants from 'expo-constants'
+import React from "react";
+import { SafeAreaView, StyleSheet, View, ImageBackground } from "react-native";
+import Constants from "expo-constants";
 
-export default function Screen({children, style }) {
-    return (
-        <SafeAreaView style={[styles.container, style]}>
-            <ImageBackground source={require('../assets/giphy.gif')} style={{width: '100%', height: '100%',  resizeMode: 'cover'}}>
-                <View style={[styles.view, style]}>
-                    {children}
-                </View>
-            </ImageBackground>
-        </SafeAreaView>
-    )
+export default function Screen({ children, style }) {
+  return (
+    <SafeAreaView style={[styles.container, style]}>
+      <ImageBackground
+        source={require("../assets/giphy.gif")}
+        style={{ width: "100%", height: "100%", resizeMode: "cover" }}
+      >
+        <View style={[styles.view, style]}>{children}</View>
+      </ImageBackground>
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        paddingTop: Constants.statusBarHeight,
-        flex: 1
-    },
-    view: {
-        // flex: 1
-    }
-})
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    flex: 1,
+  },
+  view: {
+    // flex: 1
+  },
+});
--- screens/FarmOrchardScreen.tsx
+++ screens/FarmOrchardScreen.tsx
@@ -1,18 +1,23 @@
-import React, { useEffect, useState } from 'react';
-import MapView, { PROVIDER_GOOGLE, Polyline } from 'react-native-maps'
-import { View, StyleSheet, Dimensions, Platform, Image, TouchableOpacity, Text } from 'react-native';
-import tailwind from 'tailwind-react-native-classnames';
-import { useNavigation, useIsFocused, useRoute } from '@react-navigation/native';
+import React, { useEffect, useState } from "react";
+import MapView, { Polyline } from "react-native-maps";
+import {
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  Text,
+} from "react-native";
+import tailwind from "tailwind-react-native-classnames";
+import {
+  useNavigation,
+  useRoute,
+} from "@react-navigation/native";
 
-
-import axios from 'axios';
-
+import axios from "axios";
 
 export default function FarmOrchardScreen(props) {
+  const GOOGLE_MAPS_APIKEY = "AIzaSyBBkDvVVuQBVSMOt8wQoc_7E-2bvDh2-nw";
 
-  const GOOGLE_MAPS_APIKEY = 'AIzaSyBBkDvVVuQBVSMOt8wQoc_7E-2bvDh2-nw';
-
-  const isFocused = useIsFocused();
 
   const navigation = useNavigation();
 
@@ -20,163 +25,107 @@ export default function FarmOrchardScreen(props) {
 
   const item = route.params.item;
 
-  const { id, client_id, name  } = props.route.params;
+  const { id, client_id, name } = props.route.params;
 
-  const [ orchardData, setOrchardData ] = useState([]);
+  const [orchardData, setOrchardData] = useState([]);
 
-    
   const access_token = "1566394169B0EJX2MGAVKVUGGKEMKZBMND9A7VCR";
 
-  const [ coordinatePoints, setCoordinatePoints] = useState([]);
-
+  const [coordinatePoints, setCoordinatePoints] = useState([]);
 
   const [startingPoint] = coordinatePoints;
 
- 
   const startCoordinates = {
-
-        latitude: startingPoint?.latitude,                     
-        longitude: startingPoint?.longitude, 
-        latitudeDelta: 0.005,
-        longitudeDelta: 0.005,
-
-  }
-
-
+    latitude: startingPoint?.latitude,
+    longitude: startingPoint?.longitude,
+    latitudeDelta: 0.005,
+    longitudeDelta: 0.005,
+  };
 
   const getOrchards = async () => {
-    const res = await  axios.get(`https://sherlock.aerobotics.com/developers/orchards/?farm_id=${id}`, {
-            headers: {
-                'Authorization': `${access_token}`
-              }
-              })
-              .then((res) => {
-               setOrchardData(res.data.results);
+    const res = await axios
+      .get(
+        `https://sherlock.aerobotics.com/developers/orchards/?farm_id=${id}`,
+        {
+          headers: {
+            Authorization: `${access_token}`,
+          },
+        }
+      )
+      .then((res) => {
+        setOrchardData(res.data.results);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    return res;
+  };
 
-               })
-              .catch((error) => {
-              console.error(error)
-              })
-          return res;
-  }
+  const getBoundary = async () => {
+    // filter the polygon properties from the Object
+    const geoReference = Object.keys(orchardData).reduce((result, key) => {
+      return result.concat(orchardData[key].polygon.split(" "));
+    }, []);
 
+    const lonlat = geoReference.map((coordsArr) => {
+      const longitude = coordsArr.split(",")[0];
+      const latitude = coordsArr.split(",")[1];
+      return {
+        longitude: parseFloat(longitude),
+        latitude: parseFloat(latitude),
+      };
+    });
+    setCoordinatePoints(lonlat);
+  };
 
+  console.log("start", coordinatePoints);
 
- 
-  const getBoundaris = async () =>{
+  useEffect(() => {
+    getOrchards();
 
+    getBoundary();
+  }, []);
 
-// filter the polygon properties from the Object
- const geoReference = Object.keys(orchardData).reduce((result, key) => {
-  return result.concat(orchardData[key].polygon.split(' '));
-}, [])
-
-const lonlat = geoReference.map((coordsArr) => { 
-            const longitude = coordsArr.split(',')[0];
-            const latitude = coordsArr.split(',')[1];
-            return {
-              longitude : parseFloat(longitude),
-              latitude : parseFloat(latitude),
-            }
-        },);
-        setCoordinatePoints(lonlat);
-
-      }
-
-
-console.log('start', coordinatePoints)
-        
-useEffect(()=> {
-
-  getOrchards();
-
-  getBoundaris();
-
- 
-
-}, [])  
-
-  /*   
-  const getLatLon = async () => {
-     //Converting array of Object to object 
-    const output = Object.assign({}, ...orchardData)
-    // getting the value of latitude and longitude from the polygon key/property
-    let lonlat = output.polygon;
-    // split the blank spaces into double quotes     
-    let arr = lonlat.split(' ');
-
-    try{
-      // maping the array to return the interger values of latitude and longitude   
-       const geoCoordinates = arr.map((coordsArr) => { 
-            const longitude = coordsArr.split(',')[0];
-            const latitude = coordsArr.split(',')[1];
-            return {
-              longitude : parseFloat(longitude),
-              latitude : parseFloat(latitude),
-            }  
-       }, []);
-      // return geoCoordinates;
-       const [initialregion] = geoCoordinates
-       setInitialLat(initialregion);
-    
-       setCoordinates(geoCoordinates); 
-       }
-       catch(err) {
-          console.log(err)
-        }      
-
-  }
-  */
-
- 
-
-
+  
   return (
     <View style={tailwind`flex-1 bg-white`}>
-
-        <View style={styles.container}>
-            <MapView
-             mapType="satellite"
-             region={startCoordinates}
-             style={styles.map} >
-
-            <Polyline
-              coordinates={coordinatePoints}
-              strokeColor="#FFFFFF"
-              strokeColors={['#7F0000']}
-              strokeWidth={10}
-             />
-          
-            </MapView>
-      <View>
-      <TouchableOpacity
-        onPress={getBoundaris}
-          style={
-            tailwind`h-10 w-full bg-white rounded-full items-center justify-center border border-blue-500 `
-          }
+      <View style={styles.container}>
+        <MapView
+          mapType="satellite"
+          region={startCoordinates}
+          style={styles.map}
         >
-          <Text style={tailwind`text-lg text-blue-500 font-bold`}>
-          Click to View {name} Orchards
-          </Text>
-        </TouchableOpacity> 
-</View>
+          <Polyline
+            coordinates={coordinatePoints}
+            strokeColor="#FFFFFF"
+            strokeColors={["#7F0000"]}
+            strokeWidth={10}
+          />
+        </MapView>
+        <View>
+          <TouchableOpacity
+            onPress={getBoundary}
+            style={tailwind`h-10 w-full bg-white rounded-full items-center justify-center border border-blue-500 `}
+          >
+            <Text style={tailwind`text-lg text-blue-500 font-bold`}>
+              Click to View {name} Orchards
+            </Text>
+          </TouchableOpacity>
         </View>
-
-     
+      </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    map: {
-      width: Dimensions.get('window').width,
-      height: Dimensions.get('window').height,
-    }
-   
-  })
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
+});
